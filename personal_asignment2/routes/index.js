3b969ef47ca5f65d833053dd84387d5b8077ba33
Samantha Mayes <SamanthaTmayes@gmail.com>
2026-01-17 21:57:15 -0700

    main

--- personal_asignment2/routes/index.js
+++ personal_asignment2/routes/index.js
@@ -4,7 +4,3 @@ const router = express.Router();
 router.use('/contacts', require('./contacts'))
 
 module.exports = router;
-const port= 3000;
-
-app.listen(process.env.PORT || port);
-    console.log('Web Server is listening at' + (process.env.port || port)) ;
